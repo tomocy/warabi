36927f933745712b72817fceb63c797bcc3775b4
tomocy <tomocy.dev@gmail.com>
2018-10-11 23:45:32 +0900

    fix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -73,11 +73,11 @@ func evaluateValueSpecification(spec *ast.ValueSpec) []object.Object {
 func evaluateExpression(expr ast.Expr) object.Object {
 	switch expr := expr.(type) {
 	case *ast.ParenExpr:
-		return evaluateParenExpression(expr)
+		return evaluateParenOperation(expr)
 	case *ast.BinaryExpr:
-		return evaluateBinaryExpression(expr)
+		return evaluateBinaryOperation(expr)
 	case *ast.UnaryExpr:
-		return evaluateUnaryExpression(expr)
+		return evaluateUnaryOperation(expr)
 	case *ast.Ident:
 		return evaluateIdentifier(expr)
 	case *ast.BasicLit:
@@ -87,276 +87,179 @@ func evaluateExpression(expr ast.Expr) object.Object {
 	}
 }
 
-func evaluateParenExpression(expr *ast.ParenExpr) object.Object {
+func evaluateParenOperation(expr *ast.ParenExpr) object.Object {
 	return evaluateExpression(expr.X)
 }
 
-func evaluateBinaryExpression(expr *ast.BinaryExpr) object.Object {
+func evaluateBinaryOperation(expr *ast.BinaryExpr) object.Object {
 	leftObj := evaluateExpression(expr.X)
 	rightObj := evaluateExpression(expr.Y)
 	switch {
 	case leftObj.Kind() == object.Integer && rightObj.Kind() == object.Integer:
-		return evaluateBinaryExpressionOfIntegerLiteral(leftObj.(*object.IntegerLiteral), expr.Op, rightObj.(*object.IntegerLiteral))
+		return evaluateBinaryOperationOfIntegerLiteral(
+			leftObj.(*object.IntegerLiteral),
+			expr.Op,
+			rightObj.(*object.IntegerLiteral),
+		)
 	case leftObj.Kind() == object.String && rightObj.Kind() == object.String:
-		return evaluateBinaryExpressionOfStringLiteral(leftObj.(*object.StringLiteral), expr.Op, rightObj.(*object.StringLiteral))
+		return evaluateBinaryOperationOfStringLiteral(
+			leftObj.(*object.StringLiteral),
+			expr.Op,
+			rightObj.(*object.StringLiteral),
+		)
 	case leftObj.Kind() == object.Character && rightObj.Kind() == object.Character:
-		return evaluateBinaryExpressionOfCharacterLiteral(leftObj.(*object.CharacterLiteral), expr.Op, rightObj.(*object.CharacterLiteral))
+		return evaluateBinaryOperationOfCharacterLiteral(
+			leftObj.(*object.CharacterLiteral),
+			expr.Op,
+			rightObj.(*object.CharacterLiteral),
+		)
 	case leftObj.Kind() == object.FloatingPoint && rightObj.Kind() == object.FloatingPoint:
-		return evaluateBinaryExpressionOfFloatingPointLiteral(leftObj.(*object.FloatingPointLiteral), expr.Op, rightObj.(*object.FloatingPointLiteral))
+		return evaluateBinaryOperationOfFloatingPointLiteral(
+			leftObj.(*object.FloatingPointLiteral),
+			expr.Op,
+			rightObj.(*object.FloatingPointLiteral),
+		)
 	case leftObj.Kind() == object.FloatingPoint && rightObj.Kind() == object.Integer:
 		intObj := rightObj.(*object.IntegerLiteral)
 		floatObj := &object.FloatingPointLiteral{
 			Value: float32(intObj.Value),
 		}
-		return evaluateBinaryExpressionOfFloatingPointLiteral(leftObj.(*object.FloatingPointLiteral), expr.Op, floatObj)
+		return evaluateBinaryOperationOfFloatingPointLiteral(
+			leftObj.(*object.FloatingPointLiteral),
+			expr.Op,
+			floatObj,
+		)
 	case leftObj.Kind() == object.Integer && rightObj.Kind() == object.FloatingPoint:
 		intObj := leftObj.(*object.IntegerLiteral)
 		floatObj := &object.FloatingPointLiteral{
 			Value: float32(intObj.Value),
 		}
-		return evaluateBinaryExpressionOfFloatingPointLiteral(floatObj, expr.Op, rightObj.(*object.FloatingPointLiteral))
+		return evaluateBinaryOperationOfFloatingPointLiteral(
+			floatObj,
+			expr.Op,
+			rightObj.(*object.FloatingPointLiteral),
+		)
 	default:
 		return nil
 	}
 }
 
-func evaluateBinaryExpressionOfIntegerLiteral(
-	leftObj *object.IntegerLiteral,
-	operator token.Token,
-	rightObj *object.IntegerLiteral,
-) object.Object {
-	switch operator {
-	case token.ADD, token.SUB, token.MUL, token.QUO, token.REM:
-		return evaluateArithmeticOperation(leftObj, operator, rightObj)
-	case token.LSS, token.GTR, token.LEQ, token.GEQ:
-		return evaluateRelationalOperationOfIntegerLiteral(leftObj, operator, rightObj)
-	default:
-		return nil
-	}
-}
-
-func evaluateArithmeticOperation(
+func evaluateBinaryOperationOfIntegerLiteral(
 	leftObj *object.IntegerLiteral,
 	operator token.Token,
 	rightObj *object.IntegerLiteral,
 ) object.Object {
 	switch operator {
 	case token.ADD:
-		leftObj.Value += rightObj.Value
+		return &object.IntegerLiteral{Value: leftObj.Value + rightObj.Value}
 	case token.SUB:
-		leftObj.Value -= rightObj.Value
+		return &object.IntegerLiteral{Value: leftObj.Value - rightObj.Value}
 	case token.MUL:
-		leftObj.Value *= rightObj.Value
+		return &object.IntegerLiteral{Value: leftObj.Value * rightObj.Value}
 	case token.QUO:
 		if rightObj.Value == 0 {
 			return nil
 		}
-		leftObj.Value /= rightObj.Value
+		return &object.IntegerLiteral{Value: leftObj.Value / rightObj.Value}
 	case token.REM:
-		leftObj.Value %= rightObj.Value
-	default:
-		return nil
-	}
-
-	return leftObj
-}
-
-func evaluateRelationalOperationOfIntegerLiteral(
-	leftObj *object.IntegerLiteral,
-	operator token.Token,
-	rightObj *object.IntegerLiteral,
-) object.Object {
-	var value bool
-	switch operator {
+		return &object.IntegerLiteral{Value: leftObj.Value % rightObj.Value}
 	case token.LSS:
-		value = leftObj.Value < rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value < rightObj.Value)
 	case token.GTR:
-		value = leftObj.Value > rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value > rightObj.Value)
 	case token.LEQ:
-		value = leftObj.Value <= rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value <= rightObj.Value)
 	case token.GEQ:
-		value = leftObj.Value >= rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value >= rightObj.Value)
 	default:
 		return nil
 	}
-
-	return convertToBooleanLiteral(value)
 }
 
-func evaluateBinaryExpressionOfStringLiteral(
+func evaluateBinaryOperationOfStringLiteral(
 	leftObj *object.StringLiteral,
 	operator token.Token,
 	rightObj *object.StringLiteral,
 ) object.Object {
 	switch operator {
 	case token.ADD:
-		return evaluateArithmeticOperationOfStringLiteral(leftObj, operator, rightObj)
-	case token.LSS, token.GTR, token.LEQ, token.GEQ:
-		return evaluateRelationalOperationOfStringLiteral(leftObj, operator, rightObj)
-	default:
-		return nil
-	}
-}
-
-func evaluateArithmeticOperationOfStringLiteral(
-	leftObj *object.StringLiteral,
-	operator token.Token,
-	rightObj *object.StringLiteral,
-) object.Object {
-	if operator != token.ADD {
-		return nil
-	}
-	leftObj.Value += rightObj.Value
-	return leftObj
-}
-
-func evaluateRelationalOperationOfStringLiteral(
-	leftObj *object.StringLiteral,
-	operator token.Token,
-	rightObj *object.StringLiteral,
-) object.Object {
-	var value bool
-	switch operator {
+		return &object.StringLiteral{Value: leftObj.Value + rightObj.Value}
 	case token.LSS:
-		value = leftObj.Value < rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value < rightObj.Value)
 	case token.GTR:
-		value = leftObj.Value > rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value > rightObj.Value)
 	case token.LEQ:
-		value = leftObj.Value <= rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value <= rightObj.Value)
 	case token.GEQ:
-		value = leftObj.Value >= rightObj.Value
-	default:
-		return nil
-	}
-
-	return convertToBooleanLiteral(value)
-}
-
-func evaluateBinaryExpressionOfCharacterLiteral(
-	leftObj *object.CharacterLiteral,
-	operator token.Token,
-	rightObj *object.CharacterLiteral,
-) object.Object {
-	switch operator {
-	case token.ADD, token.SUB, token.MUL, token.QUO, token.REM:
-		return evaluateArithmeticOperationOfCharacterLiteral(leftObj, operator, rightObj)
-	case token.LSS, token.GTR, token.LEQ, token.GEQ:
-		return evaluateRelationalOperationOfCharacterLiteral(leftObj, operator, rightObj)
+		return convertToBooleanLiteral(leftObj.Value >= rightObj.Value)
 	default:
 		return nil
 	}
 }
 
-func evaluateArithmeticOperationOfCharacterLiteral(
+func evaluateBinaryOperationOfCharacterLiteral(
 	leftObj *object.CharacterLiteral,
 	operator token.Token,
 	rightObj *object.CharacterLiteral,
 ) object.Object {
 	switch operator {
 	case token.ADD:
-		leftObj.Value += rightObj.Value
+		return &object.CharacterLiteral{Value: leftObj.Value + rightObj.Value}
 	case token.SUB:
-		leftObj.Value -= rightObj.Value
+		return &object.CharacterLiteral{Value: leftObj.Value - rightObj.Value}
 	case token.MUL:
-		leftObj.Value *= rightObj.Value
+		return &object.CharacterLiteral{Value: leftObj.Value * rightObj.Value}
 	case token.QUO:
 		if rightObj.Value == 0 {
 			return nil
 		}
-		leftObj.Value /= rightObj.Value
+		return &object.CharacterLiteral{Value: leftObj.Value / rightObj.Value}
 	case token.REM:
-		leftObj.Value %= rightObj.Value
-	default:
-		return nil
-	}
-
-	return leftObj
-}
-
-func evaluateRelationalOperationOfCharacterLiteral(
-	leftObj *object.CharacterLiteral,
-	operator token.Token,
-	rightObj *object.CharacterLiteral,
-) object.Object {
-	var value bool
-	switch operator {
+		return &object.CharacterLiteral{Value: leftObj.Value % rightObj.Value}
 	case token.LSS:
-		value = leftObj.Value < rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value < rightObj.Value)
 	case token.GTR:
-		value = leftObj.Value > rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value > rightObj.Value)
 	case token.LEQ:
-		value = leftObj.Value <= rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value <= rightObj.Value)
 	case token.GEQ:
-		value = leftObj.Value >= rightObj.Value
-	default:
-		return nil
-	}
-
-	return convertToBooleanLiteral(value)
-}
-
-func evaluateBinaryExpressionOfFloatingPointLiteral(
-	leftObj *object.FloatingPointLiteral,
-	operator token.Token,
-	rightObj *object.FloatingPointLiteral,
-) object.Object {
-	switch operator {
-	case token.ADD, token.SUB, token.MUL, token.QUO, token.REM:
-		return evaluateArithmeticOperationOfFloatingPointLiteral(leftObj, operator, rightObj)
-	case token.LSS, token.GTR, token.LEQ, token.GEQ:
-		return evaluateRelationalOperationOfFloatingPointLiteral(leftObj, operator, rightObj)
+		return convertToBooleanLiteral(leftObj.Value >= rightObj.Value)
 	default:
 		return nil
 	}
 }
 
-func evaluateArithmeticOperationOfFloatingPointLiteral(
+func evaluateBinaryOperationOfFloatingPointLiteral(
 	leftObj *object.FloatingPointLiteral,
 	operator token.Token,
 	rightObj *object.FloatingPointLiteral,
 ) object.Object {
 	switch operator {
 	case token.ADD:
-		leftObj.Value += rightObj.Value
+		return &object.FloatingPointLiteral{Value: leftObj.Value + rightObj.Value}
 	case token.SUB:
-		leftObj.Value -= rightObj.Value
+		return &object.FloatingPointLiteral{Value: leftObj.Value - rightObj.Value}
 	case token.MUL:
-		leftObj.Value *= rightObj.Value
+		return &object.FloatingPointLiteral{Value: leftObj.Value * rightObj.Value}
 	case token.QUO:
-		if rightObj.Value == 0.0 {
+		if rightObj.Value == 0 {
 			return nil
 		}
-		leftObj.Value /= rightObj.Value
-	default:
-		return nil
-	}
-
-	return leftObj
-}
-
-func evaluateRelationalOperationOfFloatingPointLiteral(
-	leftObj *object.FloatingPointLiteral,
-	operator token.Token,
-	rightObj *object.FloatingPointLiteral,
-) object.Object {
-	var value bool
-	switch operator {
+		return &object.FloatingPointLiteral{Value: leftObj.Value / rightObj.Value}
 	case token.LSS:
-		value = leftObj.Value < rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value < rightObj.Value)
 	case token.GTR:
-		value = leftObj.Value > rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value > rightObj.Value)
 	case token.LEQ:
-		value = leftObj.Value <= rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value <= rightObj.Value)
 	case token.GEQ:
-		value = leftObj.Value >= rightObj.Value
+		return convertToBooleanLiteral(leftObj.Value >= rightObj.Value)
 	default:
 		return nil
 	}
-
-	return convertToBooleanLiteral(value)
 }
 
-func evaluateUnaryExpression(expr *ast.UnaryExpr) object.Object {
+func evaluateUnaryOperation(expr *ast.UnaryExpr) object.Object {
 	switch expr.Op {
 	case token.SUB:
 		return evaluateMinusOperation(expr)
